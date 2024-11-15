a1c5635edc1795f821b19d4e40d7bcc6386ac953
leUnteregger <unteregger.leon@johak.at>
2024-11-15 10:09:48 +0100

    Leon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,39 @@
-<h1 class="text-5xl text-red-500">Homepage with Tailwind</h1>
+<script>
+	import { page } from '$app/stores';
+	import ListItem from '$lib/components/ListItem.svelte';
+	let { data } = $props();
+	let currentQuestion = $state(0);
+	let selectedAnswer = $state('');
+	let score = $state(0);
+	let progressPercentage = $state(10);
+	let submitted = $state(false);
 
-<!-- Open the modal using ID.showModal() method -->
-<button class="btn" onclick="my_modal_1.showModal()">...and DaisyUI (click)</button>
-<dialog id="my_modal_1" class="modal">
-	<div class="modal-box">
-		<h3 class="text-lg font-bold">Hello from Daisy 🌻!</h3>
-		<p class="py-4">Press ESC key or click the button below to close</p>
-		<div class="modal-action">
-			<form method="dialog">
-				<!-- if there is a button in form, it will close the modal -->
-				<button class="btn">Close</button>
-			</form>
-		</div>
-	</div>
-</dialog>
+	function handleSubmit() {
+		if (selectedAnswer === data.questions[currentQuestion].answer) {
+			score++;
+		}
+		progressPercentage = ((currentQuestion + 1) / data.questions.length) * 100;
+	}
+
+	function nextQuestion() {
+		currentQuestion++;
+	}
+</script>
+
+<progress class="progress progress-primary w-56" value={progressPercentage} max="100"></progress>
+
+{#each data.questions as question, index (question.question)}
+	{#if index === currentQuestion}
+		<p>Question {index + 1} of {data.questions.length}</p>
+		<h2>{question.question}</h2>
+		{#each question.options as option, index (option)}
+			<ListItem title={option} {index} focusAction={() => (selectedAnswer = option)}></ListItem>
+		{/each}
+	{/if}
+{/each}
+
+{#if !submitted}
+	<button class="btn btn-primary" onclick={handleSubmit}>Submit</button>
+{:else}
+	<button class="btn btn-primary" onclick={nextQuestion}>Next Question</button>
+{/if}
